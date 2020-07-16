39559467c0d773001f6972bb04d4fdbad4c1f222
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-16 06:05:55 -0400

    Add convenience function addDays()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from "react";
 import "./App.css";
 import {requester} from "easier-requests";
 
+function addDays(date, days) {
+  // 1000 milliseconds in a second, 60 seconds in a minute, 60 minutes
+  // in and hour, 24 hours in a day.
+  const lengthOfDay = 1000 * 60 * 60 * 24;
+
+  // get numeric value of date, then create new Date object after
+  // performing addition.
+  const dateNumber = date.getTime();
+  const newDate = new Date();
+  newDate.setTime(dateNumber + days * lengthOfDay);
+  return newDate;
+}
+
 function App() {
   return (
     <div className="App">
