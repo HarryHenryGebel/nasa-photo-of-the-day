43b3ef3612750d997eaf0e91b83789df0c7b67c3
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-16 06:06:21 -0400

    add function getAPOD() to fetch the data

--- src/App.js
+++ src/App.js
@@ -15,6 +15,31 @@ function addDays(date, days) {
   return newDate;
 }
 
+async function getAPOD (setApodData, date = undefined) {
+  const baseURL = 'https://api.nasa.gov/planetary/apod?',
+        additional = ['api_key=SrKOhrnWciCIqOHkKCsbQNBEXvjI51TD7a18iRjX',
+                      'hd=True'];
+  // If no date provided, default to today
+  if (!date)
+    date = new Date();
+
+  // add date to GET
+  additional.push(
+    `date=${date.getFullYear()}-${date.getMonth() + 1}-` +
+      `${date.getDate()}`);
+
+  const id = requester.createUniqueID();
+  await requester.get(baseURL + additional.join('&'), id);
+  const response = requester.response(id).data;
+  if (response.media_type === 'video') {
+    await getAPOD(setApodData, addDays(date, -1));
+  }
+  else {
+    console.log(response);
+    setApodData(response);
+  }
+}
+
 function App() {
   return (
     <div className="App">
