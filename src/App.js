56ca6a03a763971ace5ee65a99537a4aabf3902d
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-16 20:15:46 -0400

    Make all arguments to getAPOD() mandatory

--- src/App.js
+++ src/App.js
@@ -4,38 +4,30 @@ import {requester} from "easier-requests";
 import Header from "./Header";
 import {addDays} from "./helpers";
 
-function getAPOD (setApodData,
-                  date = undefined,
-                  setDisplayDate = undefined) {
+function getAPOD (setApodData, date, setDisplayDate) {
+  // passing async function as useEffect handler results in warning
+  // from React to only pass in a synchronous function.
   async function _getAPOD (setApodData, data, setDisplayDate) {
+    console.log(date);
     const baseURL = 'https://api.nasa.gov/planetary/apod?',
           additional = ['api_key=SrKOhrnWciCIqOHkKCsbQNBEXvjI51TD7a18iRjX',
-                        'hd=True'];
-    // If no date provided, default to today
-    if (!date)
-      date = new Date();
-
-    // add date to GET
-    additional.push(
-      `date=${date.getFullYear()}-${date.getMonth() + 1}-` +
-        `${date.getDate()}`);
+                        'hd=True',
+                        `date=${date.getFullYear()}-${date.getMonth() + 1}-`
+                        + `${date.getDate()}`];
 
     const id = requester.createUniqueID();
     await requester.get(baseURL + additional.join('&'), id);
     const response = requester.response(id).data;
 
     // if not an image, keep going back until we find an image
-    if (response.media_type === 'video') {
-      _getAPOD(setApodData, addDays(date, -1));
-    }
+    if (response.media_type === 'video')
+      setDisplayDate(setApodData, addDays(date, -1));
     else {
       // if there is no HD image, use normal as HD
       if (!('hdurl' in response))
         response.hdurl = response.url;
 
       setApodData(response);
-      if (setDisplayDate)
-        setDisplayDate(date);
     }
   }
 
@@ -78,4 +70,4 @@ function APODHolder(props) {
 
 export default App;
 
-//  LocalWords:  hdurl
+//  LocalWords:  hdurl useEffect
